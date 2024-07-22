a5efcd4d3e9f5fe4bb112d82660b4ae297e9a85b
eslammahmoud <solom6112002@gmail.com>
2024-07-22 11:50:58 +0300

    Init project. Add empty home screen. Add splash screen to light and dark mode.

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
+body: Center(
+  child: Text('weukl,ac.'),
+)
     );
   }
 }
