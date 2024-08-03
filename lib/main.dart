f38a10de75fbe6d41c8075c12b712a850ee4e798
eslammahmoud <solom6112002@gmail.com>
2024-08-03 15:03:08 +0300

    Add quran Screen. Add hadith Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
+import 'package:first_git_app/Screens/HomePage/Hadeth/hadith_details.dart';
+import 'package:first_git_app/Screens/HomePage/Quran/sura_details.dart';
 import 'package:flutter/material.dart';
-
+import 'Screens/HomePage/HomeScreen.dart';
+import 'Utlis/ThemeMode.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-body: Center(
-  child: Text('weukl,ac.'),
-)
+      theme: MyThemeData.themeMode,
+      initialRoute: '/',
+      routes: {
+        '/':(context)=>const HomePage(),
+        SuraDetails.routeName :(context) =>const SuraDetails(),
+        HadithDetails.routeName: (context) => const HadithDetails(),
+      },
     );
   }
 }
